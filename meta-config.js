45fe3eb911f9b8ebd9231ac7fc144fb659dc54f4
pej4303 <pej4303@gmail.com>
2024-08-25 00:34:57 +0900

    빌드 테스트

--- meta-config.js
+++ meta-config.js
@@ -3,9 +3,9 @@ module.exports = {
     description: `Dev Note`, // 블로그 서브명
     author: `pej4303`, 
     siteUrl: `https://pej4303.github.io/`, // 블로그 사이트 주소
-    keywords: [`안녕`, `기술`, `블로그`], 
+    keywords: [`java`, `error`, `blog`], 
     repo: 'pej4303/pej4303.github.io',
-    gtag: 'GTM-59BDNJNQ', // with. Google Analytics
+    // gtag: 'GTM-59BDNJNQ', // with. Google Analytics
     socialLinks: { 
       github: 'https://github.com/pej4303', 
       // instagram: 'https://www.instagram.com/iminseong920/', 
